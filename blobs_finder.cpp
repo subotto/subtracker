8f04068d7950a8e3a00966ec8825d2e000213c80
Giovanni Paolini <giovanni.paolini@sns.it>
2013-12-09 15:24:29 +0100

    Tolto l'header "photo.hpp", senza cui riesco a compilare.

--- blobs_finder.cpp
+++ blobs_finder.cpp
@@ -3,7 +3,6 @@
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/photo/photo.hpp"
 
 #include <iostream>
 
--- subtracker.cpp
+++ subtracker.cpp
@@ -4,7 +4,7 @@
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/photo/photo.hpp"
+//#include "opencv2/photo/photo.hpp"
 
 #include <iostream>
 #include <vector>
