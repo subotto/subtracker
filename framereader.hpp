3128baba695a0ac2a56a04ebe8c7b84bb1d2fd4c
Luca Versari <veluca93@gmail.com>
2014-01-24 15:14:10 +0100

    Miglioramenti a frame reader
    Sostituiti spazi con tab
    Droppa frames in modo più intelligente se la coda è piena
    Rimosso codice vecchio che aspettava che la coda non fosse più piena prima di inserire qualcosa nel caso di video

--- framereader.hpp
+++ framereader.hpp
@@ -24,106 +24,97 @@ struct video_clock {
 };
 
 struct frame_info {
-    time_point<video_clock> timestamp;
-    time_point<system_clock> playback_time;
-    Mat data;
+	time_point<video_clock> timestamp;
+	time_point<system_clock> playback_time;
+	Mat data;
 };
 
 class FrameReader {
 
 private:
-    deque<frame_info> queue;
-    deque<time_point<video_clock>> frame_times;
-    mutex queue_mutex;
-    condition_variable queue_not_empty;
-    condition_variable queue_not_full;
-    atomic<bool> running;
-    int count;
-    thread t;
-    VideoCapture cap;
-    time_point<system_clock> last_stats;
-    time_point<system_clock> video_start_time;
+	deque<frame_info> queue;
+	deque<time_point<video_clock>> frame_times;
+	mutex queue_mutex;
+	condition_variable queue_not_empty;
+	atomic<bool> running;
+	int count;
+	thread t;
+	VideoCapture cap;
+	time_point<system_clock> last_stats;
+	time_point<system_clock> video_start_time;
 
-    bool fromFile = false;
-    bool rate_limited = false;
+	bool fromFile = false;
+	bool rate_limited = false;
 
 public:
-    FrameReader(int device) {
-        running = true;
-        count = 0;
-        last_stats = high_resolution_clock::now();
-        cap = v4l2cap(device, 320, 240, 125);
-        t = thread(&FrameReader::read, this);
-        video_start_time = system_clock::now();
-    }
+	FrameReader(int device) {
+		running = true;
+		count = 0;
+		last_stats = high_resolution_clock::now();
+		cap = v4l2cap(device, 320, 240, 125);
+		t = thread(&FrameReader::read, this);
+		video_start_time = system_clock::now();
+	}
 
-    FrameReader(const char* file, bool rate_limited = true) {
-        running = true;
-        count = 0;
-        last_stats = high_resolution_clock::now();
-        cap = VideoCapture(file);
-        if(!cap.isOpened()) {
-            fprintf(stderr, "Cannot open video file!\n");
-            exit(1);
-        }
-        t = thread(&FrameReader::read, this);
-        fromFile = true;
-        this->rate_limited = rate_limited;
-    }
+	FrameReader(const char* file, bool rate_limited = true) {
+		running = true;
+		count = 0;
+		last_stats = high_resolution_clock::now();
+		cap = VideoCapture(file);
+		if(!cap.isOpened()) {
+			fprintf(stderr, "Cannot open video file!\n");
+			exit(1);
+		}
+		t = thread(&FrameReader::read, this);
+		fromFile = true;
+		this->rate_limited = rate_limited;
+	}
 
-    void read() {
-        if(!fromFile) {
-            // Delay di 1s per dare il tempo alla webcam di inizializzarsi
-            this_thread::sleep_for(seconds(1));
-        }
-        video_start_time = system_clock::now();
-        long unsigned int enqueued_frames = 0;
-        while(running) {
-            Mat frame;
-            if(!cap.read(frame)) {
-                if(fromFile) {
-                    fprintf(stderr, "Video ended.\n");
-                    exit(0);
-                } else {
-                    fprintf(stderr, "Error reading frame!\n");
-                    continue;
-                }
-            }
-            count++;
-            auto now = system_clock::now();
+	void read() {
+		if(!fromFile) {
+			// Delay di 1s per dare il tempo alla webcam di inizializzarsi
+			this_thread::sleep_for(seconds(1));
+		}
+		video_start_time = system_clock::now();
+		long unsigned int enqueued_frames = 0;
+		while(running) {
+			Mat frame;
+			if(!cap.read(frame)) {
+				if(fromFile) {
+					fprintf(stderr, "Video ended.\n");
+					exit(0);
+				} else {
+					fprintf(stderr, "Error reading frame!\n");
+					continue;
+				}
+			}
+			count++;
+			auto now = system_clock::now();
 
-            time_point<video_clock> timestamp;
-            if(fromFile) {
-            	double posMsec = cap.get(CV_CAP_PROP_POS_MSEC);
-            	timestamp = time_point<video_clock>(duration_cast<nanoseconds>(duration<double, milli>(posMsec)));
-            } else {
-            	timestamp = time_point<video_clock>(now - video_start_time);
-            }
+			time_point<video_clock> timestamp;
+			if(fromFile) {
+				double posMsec = cap.get(CV_CAP_PROP_POS_MSEC);
+				timestamp = time_point<video_clock>(duration_cast<nanoseconds>(duration<double, milli>(posMsec)));
+			} else {
+				timestamp = time_point<video_clock>(now - video_start_time);
+			}
 
-            frame_times.push_back(timestamp);
-            while(timestamp - frame_times.front() > frame_count_interval) {
-                frame_times.pop_front();
-            }
+			frame_times.push_back(timestamp);
+			while(timestamp - frame_times.front() > frame_count_interval) {
+				frame_times.pop_front();
+			}
 
-            if(now - last_stats > seconds(stats_interval)) {
-                fprintf(stderr, "Queue size: %lu\n",
-                    (long unsigned)queue.size());
-                fprintf(stderr, "Received %lu frames in the last %lu seconds.\n",
-                    (long unsigned) frame_times.size(),
-                    (long unsigned) seconds(frame_count_interval).count());
-                fprintf(stderr, "Processed %lu frames in %.3f seconds\n",
-                    (long unsigned) (enqueued_frames - queue.size()),
-                    float(duration_cast<duration<float>>(now - video_start_time).count()));
-                last_stats = now;
-            }
-            unique_lock<mutex> lock(queue_mutex);
-            if (fromFile) {
-                while (queue.size() >= buffer_size) {
-                    queue_not_full.wait(lock);
-                }
-            }
-
-            assert(!fromFile || queue.size() < buffer_size);
+			if(now - last_stats > seconds(stats_interval)) {
+				fprintf(stderr, "Queue size: %lu\n",
+					(long unsigned)queue.size());
+				fprintf(stderr, "Received %lu frames in the last %lu seconds.\n",
+					(long unsigned) frame_times.size(),
+					(long unsigned) seconds(frame_count_interval).count());
+				fprintf(stderr, "Processed %lu frames in %.3f seconds\n",
+					(long unsigned) (enqueued_frames - queue.size()),
+					float(duration_cast<duration<float>>(now - video_start_time).count()));
+				last_stats = now;
+			}
 
 			auto playback_time = video_start_time + timestamp.time_since_epoch();
 
@@ -131,31 +122,34 @@ public:
 				this_thread::sleep_until(playback_time);
 			}
 
-			if (queue.size() < buffer_size) {
+			unique_lock<mutex> lock(queue_mutex);
+			// Prende tutti i frames finchè queue.size() < buffer_size,
+			// un frame su 2 se queue.size() < 2*buffer_size,
+			// uno su 3 se queue.size() < 3*buffer_size, etc
+			if (count % (queue.size() / buffer_size + 1) == 0) {
 				queue.push_back( { timestamp, playback_time, frame });
 				queue_not_empty.notify_all();
 				enqueued_frames++;
 			} else {
 				fprintf(stderr, "Frame dropped!\n");
 			}
-        }
-    }
+		}
+	}
 
-    frame_info get() {
-        unique_lock<mutex> lock(queue_mutex);
-        while(queue.empty()) {
-            queue_not_empty.wait(lock);
-        }
-        auto res = queue.front();
-        queue.pop_front();
-        queue_not_full.notify_all();
-        return res;
-    }
+	frame_info get() {
+		unique_lock<mutex> lock(queue_mutex);
+		while(queue.empty()) {
+			queue_not_empty.wait(lock);
+		}
+		auto res = queue.front();
+		queue.pop_front();
+		return res;
+	}
 
-    ~FrameReader() {
-        running = false;
-        t.join();
-    }
+	~FrameReader() {
+		running = false;
+		t.join();
+	}
 };
 
 #endif
