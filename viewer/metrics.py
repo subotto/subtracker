61403e8a77b5ab6fedaa985be07910bfa2740e43
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2014-01-24 01:21:23 +0100

    Missing file.

---
+++ viewer/metrics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+FIELD_WIDTH = 1.15
+FIELD_HEIGHT = 0.7
+
+BALL_DIAMETER = 0.02
+
+ROD_DIAMETER = 0.01
+ROD_HEIGHT = 0.08
+ROD_NUMBER = 8
+ROD_DISTANCE = FIELD_WIDTH / ROD_NUMBER
+ROD_CONFIGURATION = [(1, 0.0, 0),
+                     (2, 0.2, 0),
+                     (3, 0.2, 1),
+                     (5, 0.14, 0),
+                     (5, 0.14, 1),
+                     (3, 0.2, 0),
+                     (2, 0.2, 1),
+                     (1, 0.0, 1)]
+
+CYLINDER_FACTOR = 10.0
