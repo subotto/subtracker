b0138e69c5e397a73027a1d032ebc7af06fd6e7a
Giovanni Mascellani <gio@debian.org>
2016-11-09 22:55:48 +0100

    Moar timing!

--- qt/Subtracker/frameanalysis.cpp
+++ qt/Subtracker/frameanalysis.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 using namespace chrono;
+using namespace cv;
 
 FrameAnalysis::FrameAnalysis(const cv::Mat &frame, int frame_num, const std::chrono::time_point< std::chrono::system_clock > &time, const FrameSettings &settings) :
     frame(frame), frame_num(frame_num), time(time), settings(settings) {
@@ -10,14 +11,30 @@ FrameAnalysis::FrameAnalysis(const cv::Mat &frame, int frame_num, const std::chr
 
 void FrameAnalysis::phase1() {
     this->begin_time = system_clock::now();
+    this->begin_phase1 = steady_clock::now();
+
     this->test_phase1 = this->frame;
+
+    this->end_phase1 = steady_clock::now();
 }
 
 void FrameAnalysis::phase2() {
+    this->begin_phase2 = steady_clock::now();
+
     this->test_phase2 = this->test_phase1;
+
+    this->end_phase2 = steady_clock::now();
 }
 
 void FrameAnalysis::phase3() {
+    this->begin_phase3 = steady_clock::now();
+
     this->test_phase3 = this->test_phase2;
+
+    this->end_phase3 = steady_clock::now();
     this->end_time = system_clock::now();
+}
+
+std::chrono::steady_clock::duration FrameAnalysis::total_processing_time() {
+    return (this->end_phase1 - this->begin_phase1) + (this->end_phase2 - this->begin_phase2) + (this->end_phase3 - this->begin_phase3);
 }
--- qt/Subtracker/frameanalysis.h
+++ qt/Subtracker/frameanalysis.h
@@ -13,17 +13,20 @@ class FrameAnalysis
 
 public:
     FrameAnalysis(const cv::Mat &frame, int frame_num, const std::chrono::time_point< std::chrono::system_clock > &time, const FrameSettings &settings);
+    std::chrono::steady_clock::duration total_processing_time();
+
+private:
     void phase1();
     void phase2();
     void phase3();
 
-private:
     cv::Mat frame;
     int frame_num;
     std::chrono::time_point< std::chrono::system_clock > time;
     FrameSettings settings;
 
     std::chrono::time_point< std::chrono::system_clock > begin_time, end_time;
+    std::chrono::time_point< std::chrono::steady_clock > begin_phase1, end_phase1, begin_phase2, end_phase2, begin_phase3, end_phase3;
 
     // Just to test
     cv::Mat test_phase1, test_phase2, test_phase3;
